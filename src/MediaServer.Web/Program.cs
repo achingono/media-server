ade548ea91e8b04e5684899a6df2567ad65b46f7
Alfero Chingono <2288092+achingono@users.noreply.github.com>
2024-12-13 18:55:52 +0000

    Added Web project

---
+++ src/MediaServer.Web/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.FluentUI.AspNetCore.Components;
+using MediaServer.Web.Components;
+using Microsoft.Extensions.Options;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddFluentUIComponents();
+builder.Services.AddHttpClient(Options.DefaultName, 
+        client =>
+        {
+            var baseAddress = builder.Configuration["Api:BaseAddress:Internal"];
+            if (string.IsNullOrWhiteSpace(baseAddress))
+                throw new NullReferenceException(nameof(baseAddress));
+            client.BaseAddress = new Uri(baseAddress);
+        }
+    );
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
