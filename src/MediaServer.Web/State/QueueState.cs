0397b2c6ff49b71c36de656717cfa94641a60130
Alfero Chingono <2288092+achingono@users.noreply.github.com>
2024-12-18 00:25:19 +0000

    Automatically play from track list

--- src/MediaServer.Web/State/QueueState.cs
+++ src/MediaServer.Web/State/QueueState.cs
@@ -3,6 +3,7 @@ namespace MediaServer.Web.State;
 
 public class QueueState
 {
+    public bool AutoPlay { get; set; }
     public int CurrentIndex { get; set; }
     public Entities.Track CurrentTrack { get => Items.ElementAtOrDefault(CurrentIndex); }
     public IEnumerable<Entities.Track> Items { get; set; } = Enumerable.Empty<Entities.Track>();
