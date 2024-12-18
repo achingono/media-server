6d6780668c0f6aaedf8fcd99595b903d4a14cddb
Alfero Chingono <2288092+achingono@users.noreply.github.com>
2024-12-17 22:01:09 -0500

    Added skip next and previous

--- src/MediaServer.Web/State/QueueState.cs
+++ src/MediaServer.Web/State/QueueState.cs
@@ -5,6 +5,8 @@ public class QueueState
 {
     public bool AutoPlay { get; set; }
     public int CurrentIndex { get; set; }
-    public Entities.Track CurrentTrack { get => Items.ElementAtOrDefault(CurrentIndex); }
+    public Entities.Track CurrentTrack => Items.ElementAtOrDefault(CurrentIndex)!;
+    public bool HasNext => CurrentIndex < Items.Count() - 1;
+    public bool HasPrevious => CurrentIndex > 0;
     public IEnumerable<Entities.Track> Items { get; set; } = Enumerable.Empty<Entities.Track>();
 }
